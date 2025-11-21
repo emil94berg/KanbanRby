f34f601ac91fb5ee3186530c342b7fa54602c9e9
EConrad00 <emil.conrad@campusnykoping.se>
2025-11-21 11:44:35 +0100

    feat: add models

---
+++ Models/Task.cs
@@ -0,0 +1,21 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace KanbanRby.Models;
+
+[Table("task")]
+public class Task : BaseModel
+{
+    [PrimaryKey("id")]
+    public long Id { get; set; }
+    
+    [Reference(typeof(Card))]
+    [Column("card_id")]
+    public long CardId { get; set; }
+    
+    [Column("name")]
+    public string? Name { get; set; }
+    
+    [Column("created_at")]
+    public DateTime CreatedAt { get; set; }
+}
