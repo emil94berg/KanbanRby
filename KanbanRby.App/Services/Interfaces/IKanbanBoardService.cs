2493a04da380b332041d944830765826ac4dd1cb
Oscar Lejon <oscar.lejon@campusnykoping.se>
2025-11-28 13:45:16 +0100

    Add KanbanBoardService.cs and IKanbanBoardService.cs Added as a service in Program.cs. Contains CRUD information for CrudFactory

---
+++ KanbanRby.App/Services/Interfaces/IKanbanBoardService.cs
@@ -0,0 +1,13 @@
+﻿using KanbanRby.Models;
+using Task = System.Threading.Tasks.Task;
+
+namespace KanbanRby.Services.Interfaces;
+
+public interface IKanbanBoardService
+{
+    Task<List<Kanban>> GetAllAsync();
+    Task<Kanban?> GetByIdAsync(int id);
+    Task<Kanban> CreateKanban(string name, string description);
+    Task UpdateAsync(Kanban board);
+    Task DeleteAsync(object id);
+}
