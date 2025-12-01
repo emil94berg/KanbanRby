1fa6276c3866e437daf296ea226c75c214c8ded3
Emil Berg <emil94berg@gmail.com>
2025-12-01 13:47:29 +0100

    add login page and logic

---
+++ KanbanRby.App/Services/Interfaces/ISupabaseAuthService.cs
@@ -0,0 +1,17 @@
+﻿using Supabase.Gotrue;
+using KanbanRby.Models;
+using Task = System.Threading.Tasks.Task;
+
+namespace KanbanRby.Services.Interfaces;
+
+
+public interface ISupabaseAuthService
+{
+    Task SetDisplayName(string displayName);
+    Task<User> RegisterNewAccountAsync(string email, string password);
+    Task<User?> LoginUserAsync(string email, string password);
+    Task LogoutAsync();
+    Task<User?> CurrentUserAsync();
+    Task<Session?> GetSessionTokenAsync();
+
+}
