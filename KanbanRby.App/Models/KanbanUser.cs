062887d06d05a53e3a31502c5874c286a23c99f4
Oscar Lejon <oscar.lejon@campusnykoping.se>
2025-12-02 14:07:30 +0100

    Add new join table for kanban and users

---
+++ KanbanRby.App/Models/KanbanUser.cs
@@ -0,0 +1,16 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace KanbanRby.Models;
+[Table("kanban_user")]
+public class KanbanUser : BaseModel
+{
+    [Column("user_id")]
+    public Guid UserId { get; set; }
+    
+    [Column("kanban_id")]
+    public int KanbanId { get; set; }
+    
+    [Reference(typeof(Kanban), includeInQuery: true)]
+    public Kanban KanbanDetails { get; set; }
+}
