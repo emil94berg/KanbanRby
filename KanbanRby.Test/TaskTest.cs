8c55e9e79ab64ada7653dd883405febc3dc50033
Emil Berg <emil94berg@gmail.com>
2025-12-11 10:44:39 +0100

    MyKanbans css changes, task tests

---
+++ KanbanRby.Test/TaskTest.cs
@@ -0,0 +1,29 @@
+﻿using KanbanRby.Components.Pages;
+
+namespace KanbanRby.Test;
+
+public class TaskTest
+{
+    [Fact]
+    public void ShouldChangeIsCompletedToTrueOnTask()
+    {
+        //Arrange
+        var sut = new MyKanbans();
+        var task = new Models.Task() { Name = "TestTask", IsCompleted = false };
+        //Act
+        sut.TaskCompleted(task);
+        //Assert
+        Assert.True(task.IsCompleted);
+    }
+    [Fact]
+    public void ShouldChangeIsCompletedToFaslOnTask()
+    {
+        //Arrange
+        var sut = new MyKanbans();
+        var task = new Models.Task() { Name = "TestTask", IsCompleted = true };
+        //Act
+        sut.TaskCompleted(task);
+        //Assert
+        Assert.False(task.IsCompleted);
+    }
+}
